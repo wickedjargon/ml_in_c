e923bc44ffe3825a5cb6c6612cc7ddde63582e6d
Farzin <farzineff@gmail.com>
2024-07-01 12:09:57 -0700

    first commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+set -xe
+clang -Wall -Wextra -o main main.c
---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* #include <time.h> */
+
+float train[][2] = {
+	{0, 0},
+	{1, 2},
+	{2, 4},
+	{3, 6},
+	{4, 8},
+};
+
+#define train_count sizeof(train) / sizeof(train[0])
+
+float rand_float() {
+	return (float) rand() / (float) RAND_MAX;
+}
+
+float cost(float w) {
+	float result = 0.0f;
+	for (size_t i = 0; i < train_count; ++i) {
+		float x = train[i][0];
+		float y = x*w;
+		float d = y - train[i][1];
+		result += d*d;
+	}
+	result /= (float) train_count;
+
+	return result;
+}
+
+int main() {
+	srand(69);
+	float w = rand_float() * 10.0f;
+	printf("%f\n", cost(w));
+	return 0;
+
+}
