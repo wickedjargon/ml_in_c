b678670f7a091e61f2f511d146a1854fc3a2122f
Farzin <farzineff@gmail.com>
2024-07-01 13:17:46 -0700

    refactoring

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* #include <time.h> */
+#include <time.h>
 
 float train[][2] = {
     {0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8},
@@ -10,13 +10,14 @@ float train[][2] = {
 
 float rand_float() { return (float)rand() / (float)RAND_MAX; }
 
-float cost(float w) {
+float cost(float guess) {
   float result = 0.0f;
   for (size_t i = 0; i < train_count; ++i) {
-    float x = train[i][0];
-    float y = x * w;
-    float d = y - train[i][1];
-    result += d * d;
+    float input = train[i][0];
+	float output = train[i][1];
+    float result = input * guess;
+    float difference = result - output;
+    result += difference * difference;
   }
   result /= (float)train_count;
 
@@ -24,21 +25,24 @@ float cost(float w) {
 }
 
 int main() {
-  srand(69);
-  float w = rand_float() * 10.0f;
+  srand(time(0));
+  float guess = rand_float() * 10.0f;
 
   float eps = 1e-3;
   float rate = 1e-3;
 
-  printf("%f\n", cost(w));
+  printf("%f\n", cost(guess));
 
   for (size_t i = 0; i < 1000; ++i) {
-    float dcost = (cost(w + eps) - cost(w)) / eps;
-    printf("%f\n", cost(w));
-    w -= rate * dcost;
-    printf("%f\n", cost(w));
+    float cost_derivative = (cost(guess + eps) - cost(guess)) / eps;
+    printf("%f\n", cost(guess));
+    guess -= rate * cost_derivative;
+    printf("%f\n", cost(guess));
+	if (cost(guess) <= 0.000001) {
+		break;
+	}
   }
   printf("--------------------\n");
-  printf("%f\n", w);
+  printf("%f\n", guess);
   return 0;
 }
